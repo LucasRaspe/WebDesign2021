9c82a9b00239cf9bb94500a5710454de2bdf8007
LucasRaspe <lucas.raspe22@Bcp.org>
2021-02-01 13:20:39 -0800

    First dump
    i move folders into Github

---
+++ _css/myScript.js
@@ -0,0 +1,13 @@
+const myNum=6;
+const myName= "Chris";
+alert(myName);
+
+
+<canvas id="canvas"></canvas>
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+ctx.fillStyle = 'green';
+ctx.fillRect(10, 10, 150, 100);
+const canvas = document.getElementById(canvas)
+<canvas id = "canvas" innerWidth
