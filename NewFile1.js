6956bd2203203f29e22ab7949e22ca180cd07146
LucasRaspe <77513802+LucasRaspe@users.noreply.github.com>
2021-02-21 19:02:28 -0800

    Add files via upload

---
+++ NewFile1.js
@@ -0,0 +1,106 @@
+//test code
+
+// i can initialize variables with numbers, strings, booleans etc.
+// let myNum = 6;
+// const myName = "Chris";
+// alert(myName);
+// alert(myNum);
+// myNum = 25;
+// alert(myNum);
+
+// add a canvas to the page
+
+
+// const canvas = document.getElementById('canvas');
+// const ctx = canvas.getContext('2d');
+// ctx.fillStyle = 'green';
+// ctx.fillRect(10, 10, 150, 100);
+
+// let's write a program that creates rock paper scissors logic using circle, square, triangle
+
+// gets mouse position and runs the shoot function
+
+let player;
+let mouseCoords = [];
+
+addEventListener('mousedown', mouseClick);
+
+function mouseClick(e) {
+  console.log( `
+    Screen X/Y: ${e.screenX}, ${e.screenY}
+	Client X/Y: ${e.clientX}, ${e.clientY}`);
+	mouseCoords =  [e.clientX, e.clientY];
+  
+	console.log(mouseCoords[0]);
+  if (mouseCoords[0] < getRandomInt(300)){
+    alert("I can click and get feedback from computer based on mouse position");
+  }
+}
+
+
+//initializing variables to create a canvas 
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('3d');
+
+let randNum = Math.random();
+
+console.log(randNum);
+
+if (true) {
+  alert("hello world...");
+  if(randNum > mouseCoords){
+    alert("Winner winner, chicken dinner!!!");
+  }
+}
+
+// let drawTri = true;
+// let drawCir = false;
+
+// let player = prompt("rock paper or scissors");
+// let cpu = "paper";
+
+// diScovers who won
+
+function RPS(){
+  if (player == "scissors" && cpu == "paper") {
+    drawTriangle();
+  }
+  else if(player == "rock" && cpu == "scissors"){
+    drawCircle();
+  }
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+// draws a triangle
+function drawTriangle() {
+      ctx.beginPath();
+      ctx.moveTo(75, 50);
+      ctx.lineTo(100, 75);
+      ctx.lineTo(100, 25);
+      ctx.fill();
+    }
+
+  function drawSquare(){
+    ctx.fillRect(25, 25, 100, 100);
+    ctx.clearRect(45, 45, 60, 60);
+    ctx.strokeRect(50, 50, 50, 50);
+  }
+  function drawCircle(){
+    //ctx.beginPath();
+    ctx.arc(100, 75, 50, 0, 2 * Math.PI);
+    ctx.stroke();
+  }
+  d = new Date();
+
+  function main(){
+  //  RPS();
+  
+  }
+
+main();
+ 
+
+
